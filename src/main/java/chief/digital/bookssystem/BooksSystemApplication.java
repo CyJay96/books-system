62a077f832fb4b5d49c43fc2aa5cb0d0f415045b
CyJay96 <konstantin201586@gmail.com>
2023-06-19 17:42:54 +0300

    initial commit

---
+++ src/main/java/chief/digital/bookssystem/BooksSystemApplication.java
@@ -0,0 +1,12 @@
+package chief.digital.bookssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksSystemApplication.class, args);
+    }
+}
---
+++ src/test/java/chief/digital/bookssystem/BooksSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package chief.digital.bookssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
